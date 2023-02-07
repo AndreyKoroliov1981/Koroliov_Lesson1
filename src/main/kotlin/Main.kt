41150c717b770775531e9633a98437a3c50fe88e
Andrey Koroliov <korol81@bk.ru>
2023-02-07 08:44:03 +0300

    add function shuffleListStudent, and add skope-function -> also

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import models.Student
 
 fun main() {
     println("Hello Aston!")
-    val listStudent = createListStudent()
+    var listStudent = createListStudent()
     println(listStudent)
     var key: Int
     do {
@@ -12,13 +12,18 @@ fun main() {
         println("0 - выход, ")
         println("1 - распечатать список студентов,")
         println("2 - распечатать список студентов парней,")
-        println("3 - распечатать список студентов девушек:")
+        println("3 - распечатать список студентов девушек,")
+        println("4 - перемешать список студентов:")
         key = readlnOrNull()?.toIntOrNull() ?: return
-        if ((key < 0) || (key > 3)) println("Incorrect n, enter n >= 0, and n < 2") else {
+        if ((key < 0) || (key > 4)) println("Incorrect n, enter n >= 0, and n < 2") else {
             when (key) {
                 1 -> printListStudent(listStudent)
                 2 -> printListStudentMale(listStudent)
                 3 -> printListStudentFemale(listStudent)
+                4 -> shuffleListStudent(listStudent)?.let {
+                    println("перемешать список студентов удалось")
+                    listStudent = it
+                }
             }
         }
 
@@ -28,10 +33,19 @@ fun main() {
 
 fun printListStudent(listStudent: List<Student>) {
     for (i in listStudent) {
-        with (i) {
+        with(i) {
             println("Студент $name из $city, пол - $sex ")
         }
     }
+}
+
+fun shuffleListStudent(listStudent: List<Student>): List<Student>? {
+    if (listStudent.size % 2 != 0) return null
+    val newListStudent = listStudent.toMutableList()
+    for (i in 1..listStudent.size / 2) {
+        newListStudent[i*2-2] = newListStudent[i*2-1].also { newListStudent[i*2-1] = newListStudent[i*2-2] }
+    }
+    return newListStudent.toList()
 }
 
 
@@ -39,18 +53,20 @@ fun printListStudentMale(listStudent: List<Student>) {
     for (i in listStudent) {
         when (i.sex) {
             is Male -> println(i)
-            else  -> {}
+            else -> {}
         }
     }
 }
+
 fun printListStudentFemale(listStudent: List<Student>) {
     for (i in listStudent) {
         when (i.sex) {
             is Female -> println(i)
-            else  -> {}
+            else -> {}
         }
     }
 }
+
 fun createListStudent(): List<Student> {
     val answer = mutableListOf<Student>()
     answer.add(Student("Вадим", "Уфа", Male()))
